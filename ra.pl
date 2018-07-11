967e7e1e5ae61c6e799fc982e6011fb4235a096b
Ivan Krylov <krylov.r00t@gmail.com>
2018-07-11 17:49:04 +0300

    don't fiddle with STDOUT when there may be a separate filehandle

--- ra.pl
+++ ra.pl
@@ -5,34 +5,33 @@ use autodie;
 use URI;
 use HTML::TreeBuilder 5 -weak;
 
-close STDOUT;
-open STDOUT, ">:utf8", 'ra.html';
-
-binmode STDERR, ":utf8";
+open my $html, ">:utf8", 'ra.html';
+binmode STDOUT, ":utf8";
 
 my $url = 'https://qntm.org/ra';
-
 # keep a reference to the root of the tree
 my $idx = HTML::TreeBuilder::->new_from_url($url);
 
 # make the chapter structure make sense
-say HTML::Element::->new_from_lol(['a', { name => 'ra' }])->as_HTML;
-say HTML::Element::->new_from_lol(['h1', 'Ra'])->as_HTML;
+say $html HTML::Element::->new_from_lol(['a', { name => 'ra' }])->as_HTML;
+say $html HTML::Element::->new_from_lol(['h1', 'Ra'])->as_HTML;
 
-for my $item ($idx->look_down(_tag => 'div', id => 'content')->look_down(sub { $_[0]->tag =~ /^(h[34]|a)$/i	})) {
+for my $item ($idx->look_down(_tag => 'div', id => 'content')->look_down(sub { $_[0]->tag =~ /^(h[34]|a)$/i })) {
 	if ($item->tag eq 'h3') {
 		if ($item->attr('id') eq 'sec0') { next } # skip "contents"
 		else { last } # stop once we've reached something else
 	} elsif ($item->tag eq 'h4') {
 		$item->tag('h1');
-		say $item->as_HTML;
+		say $html $item->as_HTML;
 	} else { # must be a link
 		say STDERR $item->as_trimmed_text, " ", $item->attr('href');
 		# allow anchor-linking between chapters
-		say HTML::Element::->new_from_lol(['a', { name => $item->attr('href') =~ s/[^a-z]//gr }])->as_HTML;
-		say HTML::Element::->new_from_lol(['h2', $item->as_trimmed_text])->as_HTML;
+		say $html HTML::Element::->new_from_lol(['a', { name => $item->attr('href') =~ s/[^a-z]//gr }])->as_HTML;
+		say $html HTML::Element::->new_from_lol(['h2', $item->as_trimmed_text])->as_HTML;
+
 		# must keep this root, too
 		my $ch = HTML::TreeBuilder::->new_from_url(URI::->new_abs($item->attr('href'), $url));
+
 		my $chapter = $ch->look_down(_tag => "div", id => "content");
 		# remove links to previous/next chapter
 		for (($chapter->content_list)[0,-1]) {
@@ -45,8 +44,11 @@ for my $item ($idx->look_down(_tag => 'div', id => 'content')->look_down(sub { $
 				$_->attr('href' => "#". ($_->attr('href') =~ s/[^a-z]//gr));
 			}
 		}
-		say $chapter->as_HTML;
+
+		say $html $chapter->as_HTML;
 	}
 }
 
-system qw(ebook-convert ra.html ra.epub --title=Ra --authors=qntm --language=en --input-encoding=utf8 --max-levels=0 --no-default-epub-cover), q{--chapter=//*[(name()='h1' or name()='h2')]}, q{--level1-toc=//*[name()='h1']}, q{--level2-toc=//*[name()='h2']}, q{--level3-toc=//*[name()='h3']};
+$html->flush();
+
+exec qw(ebook-convert ra.html ra.epub --title=Ra --authors=qntm --language=en --input-encoding=utf8 --max-levels=0 --no-default-epub-cover), q{--chapter=//*[(name()='h1' or name()='h2')]}, q{--level1-toc=//*[name()='h1']}, q{--level2-toc=//*[name()='h2']}, q{--level3-toc=//*[name()='h3']};
